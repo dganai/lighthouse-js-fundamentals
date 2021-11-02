fec67064b6bca3982c19546adf413a4eadea5f35
dganai <dinaganai@gmail.com>
2021-11-02 01:23:23 +0000

    Anonymous function expression and named function expression

---
+++ hello.js
@@ -0,0 +1,17 @@
+/*  const sayHello = function () {
+  console.log("Hello, world");
+};
+
+sayHello();
+
+*/
+
+//next example//
+
+const sayHello = function (name) {
+  //funcion still takes a parameter -> name//
+  console.log("Hello, " + name); //function expression; storing function in variable//
+};
+sayHello("Dina");
+sayHello("Marc");
+sayHello("Preet");
