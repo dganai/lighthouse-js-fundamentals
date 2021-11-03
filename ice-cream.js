08a9d34aa8f0886dbdeb3ff6cd593ed6c7c1942d
dganai <dinaganai@gmail.com>
2021-11-03 15:46:02 +0000

    Challenge: using array properties

---
+++ ice-cream.js
@@ -0,0 +1,30 @@
+// Do not modify these first two lines
+const iceCreamFlavours = [
+  "chocolate",
+  "vanilla",
+  "cookies and cream",
+  "rocky road",
+  "strawberry",
+];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+// Do not modify these first two lines
+let iceCreamFlavours = [
+  "chocolate",
+  "vanilla",
+  "cookies and cream",
+  "rocky road",
+  "strawberry",
+];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours); //addition of root beer to end of array//
+
+console.log(iceCreamFlavours[0]); //first flavour in array//
+
+console.log(iceCreamFlavours[5]); //last flavour in array//
+
+console.log(iceCreamFlavours.length); // number of items in the array//
